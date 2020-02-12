3f6a22c6b2926f86ea972c7f71c05bcd7baf66af
piotr <PiotrBosak@yahoo.com>
2020-02-12 18:00:56 +0100

    added POJOs

---
+++ src/main/java/pb/spring/mypetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package pb.spring.mypetclinic.model;
+
+public class Owner extends Person {
+    public Owner(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
---
+++ src/main/java/pb/spring/mypetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package pb.spring.mypetclinic.model;
+
+public class Vet extends Person {
+
+    public Vet(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
