a41d5a251c08d639614d139853abce7f8efa4894
piotr <PiotrBosak@yahoo.com>
2020-02-15 13:20:26 +0100

    Listed Owners on Index Page. closes #16

--- pet-clinic-web/src/main/java/pb/spring/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pb/spring/mypetclinic/bootstrap/DataLoader.java
@@ -36,6 +36,13 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Owner owner3 = new Owner();
+        owner3.setId(3L);
+        owner3.setFirstName("twoj");
+        owner3.setLastName("stary");
+
+        ownerService.save(owner3);
+
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
--- pet-clinic-web/src/main/java/pb/spring/mypetclinic/controllers/OwnerIndexController.java
+++ pet-clinic-web/src/main/java/pb/spring/mypetclinic/controllers/OwnerIndexController.java
@@ -1,13 +1,23 @@
 package pb.spring.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pb.spring.mypetclinic.services.OwnerService;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerIndexController {
 
+    private final OwnerService ownerService;
+
+    public OwnerIndexController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/index","/index.html","/"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
