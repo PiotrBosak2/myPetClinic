2c16da6d577ab13fcebf6a6679cdcac7d4e63e6a
piotr <PiotrBosak@yahoo.com>
2020-02-18 12:27:11 +0100

    added Visit Closes #26

--- pet-clinic-data/src/main/java/pb/spring/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pb/spring/mypetclinic/model/Owner.java
@@ -1,10 +1,21 @@
 package pb.spring.mypetclinic.model;
 
-public class Owner extends Person {
-    public Owner(){
+import java.util.Set;
 
+public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Owner(){
     }
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
