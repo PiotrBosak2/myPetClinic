aee44443a0187a09dee7c337bb634788d83eb1a1
piotr <PiotrBosak@yahoo.com>
2020-03-05 20:01:58 +0100

    updated Visit,Pet

--- pet-clinic-data/src/main/java/pb/spring/mypetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pb/spring/mypetclinic/model/Visit.java
@@ -1,14 +1,25 @@
 package pb.spring.mypetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
-
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    public Visit(){
+
+    }
+
+    @Column(name = "date")
     private LocalDate date;
 
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public Visit(LocalDate date, String description, Pet pet) {
